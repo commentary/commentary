7bbc14a3f7a8a8a63f40fd3d816dd80e85450d89
Matt Rubin <mattrubin@me.com>
2012-12-01 18:19:21 -0800

    logging in and out

--- js/api.js
+++ js/api.js
@@ -9,6 +9,8 @@ window.APPDOTNET = (function () {
 
     var API = {
 
+        /* Before init you need to get an access token */
+        /* Think of this like a static function versus a class method */
         get_auth_url: function (options) {
             var local_options = $.extend({}, default_options, options);
             var url = URI('https://' + local_options.auth_host + local_options.auth_endpoint);
@@ -19,6 +21,95 @@ window.APPDOTNET = (function () {
             url.addSearch('scope', local_options.scope);
 
             return '' + url;
+        },
+
+        init: function (options) {
+            this.options = $.extend({}, default_options, options);
+            this.options.root_url = 'https://' + this.options.api_host + this.options.url_base;
+
+            if (!this.options.access_token) {
+                throw 'You must initialize the API with an access_token or it wont work';
+            }
+
+            if (this.options.no_globals) {
+                delete window.APPDOTNET;
+            }
+
+            return this;
+        },
+
+        request: function (location, ajax_options) {
+            ajax_options.data = ajax_options.data || {};
+
+            ajax_options.url = this.options.root_url + location;
+            ajax_options.data.access_token = this.options.access_token;
+
+            return $.ajax(ajax_options);
+        },
+
+        users: function (user_id) {
+            user_id = user_id || 'me';
+            var options = {
+                type: 'GET'
+            };
+
+            var url = 'users/' + user_id;
+
+            return this.request(url, options);
+        },
+
+        mentions: function (user_id) {
+            user_id = user_id || 'me';
+            var options = {
+                type: 'GET'
+            };
+
+            var url = 'users/' + user_id + '/mentions';
+
+            return this.request(url, options);
+        },
+
+        posts: function (text, reply_to) {
+            var options = {
+                type: 'POST',
+                data: {
+                    text: text
+                }
+            };
+
+            if (reply_to) {
+                options.data.reply_to = reply_to;
+            }
+
+            return this.request('posts', options);
+        },
+
+        delete_post: function (post_id) {
+            var options = {
+                type: 'DELETE'
+            };
+
+            var url = 'posts/' + post_id;
+
+            return this.request(url, options);
+        },
+
+        follows: function (user_id, new_state) {
+            var options = {
+                data: {
+                    user_id: user_id
+                }
+            };
+            if (new_state === 1) {
+                // performing a follow
+                options.type = 'POST';
+            } else if (new_state === 0) {
+                // performing an unfollow
+                options.type = 'DELETE';
+            } else {
+                throw "Invalid follow state.";
+            }
+            return this.request('users/' + user_id + '/follow', options);
         }
 
     };
--- js/commentary.js
+++ js/commentary.js
@@ -4,6 +4,8 @@
 
     var ask_for_authentication = function () {
         console.log('Initializing authentication...');
+        $('#authenticate').removeClass('hide');
+
         $('#authenticate').on('click', function () {
             console.log('authenticating...');
             var url = API.get_auth_url(NOTIFIER_OPTIONS);
@@ -11,16 +13,43 @@
         });
     };
 
-    // if (window.location.hash !== '' && !window.localStorage.access_token) {
-    //     var keys = URI('?' + window.location.hash.slice(1)).query(true);
-    //     if (keys && keys.access_token) {
-    //         window.localStorage.access_token = keys.access_token;
-    //     }
-    // }
+    var init = function (){
+        NOTIFIER_OPTIONS.access_token = window.localStorage.access_token;
+        API = API.init(NOTIFIER_OPTIONS);
+
+        $('#logout').on('click', function () {
+            console.log('Logging out...');
+            delete window.localStorage.access_token;
+        });
+        $('#logout').addClass("btn");
+
+
+        var get_user_info = function () {
+            $('#authenticate').addClass('hide');
+
+            API.users().done(function (data) {
+                $('#userinfo').find('.user_full_name').text(data.data.name);
+                $('#userinfo').removeClass('hide');
+            });
+        }
+
+        get_user_info();
+    }
+
+    
+
     var check_access_token = function () {
+        if (window.location.hash !== '' && !window.localStorage.access_token) {
+            var keys = URI('?' + window.location.hash.slice(1)).query(true);
+            if (keys && keys.access_token) {
+                window.localStorage.access_token = keys.access_token;
+            }
+        }
+
         console.log("Checking access token...");
         if (window.localStorage.access_token) {
             console.log("...user is authenticated");
+            init();
         } else {
             console.log("...not authenticated");
             ask_for_authentication();
